5b239918c6c6edd72999ccd1cf9d9da9b0c77823
Matt Peter <mhpeter97@gmail.com>
2018-05-08 23:35:36 -0400

    Create removeFirefoxRestrictions.sh
    Shell for removing Firefox restrictions

---
+++ spikes/Browser Features/removeFirefoxRestrictions.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Revert the Mozilla Firefox browser to previous settings
+# 
+
+echo "Script to revert Mozilla Firefox browser to previous settings"
+
+PROFDIR=$(cat ~/.mozilla/firefox/profiles.ini | grep 'Path=' | sed s/^Path=//)
+cd ~/.mozilla/firefox/$PROFDIR
+
+rm user.js
+touch user.js
+echo "user_pref(\"browser.tabs.closeWindowWithLastTab\", true);" >> user.js
+echo "user_pref(\"full-screen-api.enabled\", true);" >> user.js
+echo "user_pref(\"extensions.pocket.enabled\", true);" >> user.js
+
+rm -r chrome
+mkdir chrome
+cd chrome
+touch userContent.css
+touch userChrome.css
+
+
+
+echo "Mozilla Firefox browser reverted to previous settings"
