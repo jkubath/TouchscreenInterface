dd113c51d352a46989b7b1e3844a318a8ef6c588
Matt Peter <mhpeter97@gmail.com>
2018-05-18 21:13:45 -0400

    Update removeFirefoxRestrictions.sh
    Had to put a space before the -e for it to work

--- spikes/Browser Features/Firefox Restrictions/removeFirefoxRestrictions.sh
+++ spikes/Browser Features/Firefox Restrictions/removeFirefoxRestrictions.sh
@@ -7,7 +7,7 @@ echo "Script to revert Mozilla Firefox browser to previous settings"
 PROFDIR=$(cat ~/.mozilla/firefox/profiles.ini | grep 'Path=' | sed s/^Path=//)
 cd ~/.mozilla/firefox/$PROFDIR
 
-[-e user.js ] && rm user.js
+[ -e user.js ] && rm user.js
 touch user.js
 echo "user_pref(\"browser.tabs.closeWindowWithLastTab\", true);" >> user.js
 echo "user_pref(\"full-screen-api.enabled\", true);" >> user.js
