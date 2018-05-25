204417cec0f7781bcd1d18c1e1baada20d941630
Matt Peter <mhpeter97@gmail.com>
2018-05-24 22:41:25 -0400

    Added search, privacy, and security restrictions
    Added reverse of addFirefoxRestrictions.sh

--- spikes/Browser Features/Firefox Restrictions/removeFirefoxRestrictions.sh
+++ spikes/Browser Features/Firefox Restrictions/removeFirefoxRestrictions.sh
@@ -16,6 +16,17 @@ echo "user_pref(\"browser.tabs.remote.separateFileUriProcess\", true);" >> user.
 echo "user_pref(\"browser.download.folderList\", 1);" >> user.js
 echo "user_pref(\"browser.download.forbid_open_with\", false);" >> user.js
 echo "user_pref(\"browser.download.dir\", \"~/Downloads\");" >> user.js
+echo "user_pref(\"privacy.trackingprotection.enabled\", false);" >> user.js
+echo "user_pref(\"privacy.donnottrackheader.enabled\", false);" >> user.js
+echo "user_pref(\"extensions.formatautofill.addresses.enabled\", true);" >> user.js
+echo "user_pref(\"signon.rememberSignons\", true);" >> user.js
+echo "user_pref(\"network.cookie.cookieBehavior\", 0);" >> user.js
+echo "user_pref(\"browser.urlbar.suggest.bookmark\", true);" >> user.js
+echo "user_pref(\"browser.urlbar.suggest.history\", true);" >> user.js
+echo "user_pref(\"browser.urlbar.suggest.searches\", true);" >> user.js
+echo "user_pref(\"privacy.history.custom\", false);" >> user.js
+echo "user_pref(\"places.history.enabled\", true);" >> user.js
+echo "user_pref(\"browser.formfill.enable\", true);" >> user.js
 
 [ -d chrome ] && rm -r chrome
 mkdir chrome
