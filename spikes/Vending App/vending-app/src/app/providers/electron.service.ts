7b8665b2684b1fcf61f340f372fe698916ce1661
Michael <michael.riess@mailfence.com>
2018-10-15 17:09:53 -0400

    started building testing env

--- spikes/Vending App/vending-app/src/app/providers/electron.service.ts
+++ spikes/Vending App/vending-app/src/app/providers/electron.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 
+
 // If you import a module but never use any of the imported values other than as TypeScript types,
 // the resulting javascript file will look as if you never imported the module at all.
 import { ipcRenderer, webFrame, remote } from 'electron';
@@ -9,26 +10,40 @@ import * as fs from 'fs';
 @Injectable()
 export class ElectronService {
 
-  ipcRenderer: typeof ipcRenderer;
-  webFrame: typeof webFrame;
-  remote: typeof remote;
-  childProcess: typeof childProcess;
-  fs: typeof fs;
+    ipcRenderer: typeof ipcRenderer;
+    webFrame: typeof webFrame;
+    remote: typeof remote;
+    childProcess: typeof childProcess;
+    fs: typeof fs;
 
-  constructor() {
-    // Conditional imports
-    if (this.isElectron()) {
-      this.ipcRenderer = window.require('electron').ipcRenderer;
-      this.webFrame = window.require('electron').webFrame;
-      this.remote = window.require('electron').remote;
+    constructor() {
 
-      this.childProcess = window.require('child_process');
-      this.fs = window.require('fs');
+        // Conditional imports
+        if (this.isElectron()) {
+            this.ipcRenderer = window.require('electron').ipcRenderer;
+            this.webFrame = window.require('electron').webFrame;
+            this.remote = window.require('electron').remote;
+
+            this.childProcess = window.require('child_process');
+            this.fs = window.require('fs');
+        } else {
+            this.fs = {
+                writeFile: () => {},
+                writeFileSync: () => { },
+                readFile: () => { },
+                readFileSync: () => {
+                    return '{}';
+                },
+                readdir: () => { },
+                readdirSync: () => {
+                    return '';
+                }
+            } as any;
+        }
     }
-  }
 
-  isElectron = () => {
-    return window && window.process && window.process.type;
-  }
+    isElectron = () => {
+        return window && window.process && window.process.type;
+    }
 
 }
