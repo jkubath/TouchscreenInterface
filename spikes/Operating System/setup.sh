5068a58160b1f2d2921eb36af96624a53aed48a4
jonah <jkubath1@gmail.com>
2018-11-05 23:47:24 -0500

    Setup.sh fixed

--- spikes/Operating System/setup.sh
+++ spikes/Operating System/setup.sh
@@ -1,24 +1,19 @@
 #!/usr/bin/env bash
 
+# Root directory to download and build the application
+ROOT_DIR = "~/Desktop"
+
 echo "Installing dependency packages"
 sudo apt-get install -y curl
 sudo curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 
 sudo apt-get install -y nodejs build-essential wmctrl git sqlite3
 
-cd ~/Desktop
+# Download the git repository
+cd $ROOT_DIR
 git clone https://github.com/jkubath/TouchscreenInterface
 
-cd ~/Desktop/TouchscreenInterface/spikes
-make deploy
-cd ~/Desktop/TouchscreenInterface/src
-chmod -R 777 ./vending-app
+# Copy the src from spikes to ./build, compile app, copy needed files to ./deploy
+cd $ROOT_DIR/TouchscreenInterface
+make deploy1
 
-npm install
-
-cd ~/Desktop/TouchscreenInterface/src/setup/start
-./addFirefoxAddons.sh
-
-
-
-echo "Finished"
